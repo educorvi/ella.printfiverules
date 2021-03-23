e7b299113ce1fcedf12d98293ff3fd534e2301c0
Seppo Walther <seppo.walther@educorvi.de>
2021-03-23 18:38:34 +0100

    Fixed bug

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     packages = find_packages(),
-    package_data = {'': ['*.jpg', '*.ttf']}
+    package_data = {'': ['*.jpg', '*.ttf']},
     install_requires=[
     'fpdf',
     ]
