8078331b52f39b17e9ddf68d3566821b637ee96d
Seppo Walther <seppo.walther@educorvi.de>
2021-03-25 18:58:23 +0100

    Edited pdfprinter.py

--- src/printfiverules/pdfprinter.py
+++ src/printfiverules/pdfprinter.py
@@ -1,6 +1,46 @@
+from .s136 import create_pdf as s136_pdf
+from .s137 import create_pdf as s137_pdf
+from .s138 import create_pdf as s138_pdf
+from .s139 import create_pdf as s139_pdf
+from .s140 import create_pdf as s140_pdf
+from .s141 import create_pdf as s141_pdf
+from .s142 import create_pdf as s142_pdf
 from .s143 import create_pdf as s143_pdf
+from .s144 import create_pdf as s144_pdf
+from .s145 import create_pdf as s145_pdf
+from .s146 import create_pdf as s146_pdf
 
 class PdfPrinter(object):
+    def s136(self, data):
+        pdf = s136_pdf(data)
+        return pdf
+    def s137(self, data):
+        pdf = s137_pdf(data)
+        return pdf
+    def s138(self, data):
+        pdf = s138_pdf(data)
+        return pdf
+    def s139(self, data):
+        pdf = s139_pdf(data)
+        return pdf
+    def s140(self, data):
+        pdf = s140_pdf(data)
+        return pdf
+    def s141(self, data):
+        pdf = s141_pdf(data)
+        return pdf
+    def s142(self, data):
+        pdf = s142_pdf(data)
+        return pdf
     def s143(self, data):
         pdf = s143_pdf(data)
+        return pdf
+    def s144(self, data):
+        pdf = s144_pdf(data)
+        return pdf
+    def s145(self, data):
+        pdf = s145_pdf(data)
+        return pdf
+    def s146(self, data):
+        pdf = s146_pdf(data)
         return pdf
