3140cabcbe084a16e3c307186896ee24949071bb
Seppo Walther <seppo.walther@educorvi.de>
2021-03-27 14:37:24 +0100

    Added docid to s136.py

--- src/printfiverules/s136.py
+++ src/printfiverules/s136.py
@@ -20,6 +20,7 @@ def create_pdf(input):
     pdf.image(str(template2page1path), x=-4, y=-8, w=217, h=313)
 
     data = {}
+    docid = input.get("docid")
     input = input.get("data")
 
     # Kopffragen
@@ -295,7 +296,7 @@ def create_pdf(input):
             pdf.set_xy(12.7, 256.5)
             pdf.cell(0, 0, str(data.get("entfernung") + " Meter"))
 
-    pdf.output("s136.pdf", "F")
+    return pdf.output('%s.pdf' % docid)
 
 if __name__ == "__main__":
     from importdata import zaehleranlagen as input
