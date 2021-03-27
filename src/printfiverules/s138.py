acf0353fc9b5548d3ef3f47929c08fd45bd09121
Seppo Walther <seppo.walther@educorvi.de>
2021-03-27 14:40:04 +0100

    Added docid to s138.py

--- src/printfiverules/s138.py
+++ src/printfiverules/s138.py
@@ -20,6 +20,7 @@ def create_pdf(input):
     pdf.image(str(template3page1path), x=-4, y=-8, w=217, h=313)
 
     data = {}
+    docid = input.get("docid")
     input = input.get("data")
 
     # Kopffragen
@@ -491,7 +492,7 @@ def create_pdf(input):
             pdf.set_xy(12.7, 160.5)
             pdf.cell(0, 0, str(data.get("entfernung") + " Meter"))
 
-    pdf.output("s138.pdf", "F")
+    return pdf.output('%s.pdf' % docid)
 
 if __name__ == "__main__":
     from importdata import evu_niederspannungskabel as input
