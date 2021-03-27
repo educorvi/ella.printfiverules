0cce7330038e02779e2d1b3050df37179d503e3a
Seppo Walther <seppo.walther@educorvi.de>
2021-03-27 14:33:54 +0100

    Added docid to s143.py

--- src/printfiverules/s143.py
+++ src/printfiverules/s143.py
@@ -20,6 +20,7 @@ def create_pdf(input):
     pdf.image(str(template1page1path), x=-4, y=-8, w=217, h=313)
 
     data = {}
+    docid = input.get("docid")
     input = input.get("data")
 
     #Kopffragen
@@ -309,7 +310,7 @@ def create_pdf(input):
     pdf.set_xy(12.7, 253.1)
     pdf.cell(0, 0, data.get("art_der_abdeckung"))
 
-    return pdf.output("s143.pdf", "S")
+    return pdf.output('%s.pdf' % docid)
 
 if __name__ == "__main__":
     from importdata import elektrohandwerk_schaltanlagen as input
